b7d30fe6f3f0c4856af68e2a16980b043a7b21ae
Alicia Amalia Alvarez Mon <alicia_suerte@hotmail.com>
2017-04-18 01:48:18 -0300

    ahora anda el 6

--- entregable/simusched/sched_PSJF.cpp
+++ entregable/simusched/sched_PSJF.cpp
@@ -110,24 +110,149 @@ int SchedPSJF::tick(int cpu, const enum Motivo m) //NUNCA VA A SER BLOCK!!
 			}				
 			}else{
 				int prioridad = (*(tsk_params(current_pid(cpu))))[0];
-				for(int i =1; i <= prioridad; i++){
+				int duracion = (*(tsk_params(current_pid(cpu))))[1];		
+				if(prioridad == 1){
+					if(!mypq1.empty() and get<0>(mypq1.top()) < duracion){	
+						int next = get<1>(mypq1.top());
+						mypq1.pop();
+						mypq1.push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));						
+						return next;
+					}else{
+						return current_pid(cpu);
+					}
+				}
+				if(prioridad == 2){
+					if(!mypq1.empty()){
+						int next = get<1>(mypq1.top());
+						mypq1.pop();
+						mypq2.push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));						
+						return next;
+					}else{
+						if(!mypq2.empty() and get<0>(mypq2.top()) < duracion){	
+							int next = get<1>(mypq2.top());
+							mypq2.pop();
+							mypq2.push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));						
+							return next;
+						}else{
+							return current_pid(cpu);
+						}
+					}
+				}
+				if(prioridad == 3){
+					if(!mypq1.empty()){
+						int next = get<1>(mypq1.top());
+						mypq1.pop();
+						mypq3.push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));						
+						return next;
+					}else{
+						if(!mypq2.empty()){
+							int next = get<1>(mypq2.top());
+							mypq2.pop();
+							mypq3.push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));						
+							return next;
+						}else{
+							if(!mypq3.empty() and get<0>(mypq3.top()) < duracion){	
+								int next = get<1>(mypq3.top());
+								mypq3.pop();
+								mypq3.push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));						
+								return next;
+							}else{
+								return current_pid(cpu);
+							}
+						}
+					}
+				}
+				if(prioridad == 4){					
+					if(!mypq1.empty()){
+						int next = get<1>(mypq1.top());
+						mypq1.pop();
+						mypq4.push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));						
+						return next;
+					}else{
+						if(!mypq2.empty()){
+							int next = get<1>(mypq2.top());
+							mypq2.pop();
+							mypq4.push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));						
+							return next;
+						}else{
+							if(!mypq3.empty()){
+								int next = get<1>(mypq3.top());
+								mypq3.pop();
+								mypq4.push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));						
+								return next;
+							}else{							
+								if(!mypq4.empty() and get<0>(mypq4.top()) < duracion){	
+									int next = get<1>(mypq4.top());
+									mypq4.pop();
+									mypq4.push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));						
+									return next;
+								}else{
+									return current_pid(cpu);
+								}
+							}
+						}
+					}
+				}
+				if(prioridad == 5){
+					if(!mypq1.empty()){
+						int next = get<1>(mypq1.top());
+						mypq1.pop();
+						mypq5.push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));						
+						return next;
+					}else{
+						if(!mypq2.empty()){
+							int next = get<1>(mypq2.top());
+							mypq2.pop();
+							mypq5.push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));						
+							return next;
+						}else{
+							if(!mypq3.empty()){
+								int next = get<1>(mypq3.top());
+								mypq3.pop();
+								mypq5.push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));						
+								return next;
+							}else{	
+								if(!mypq4.empty()){
+									int next = get<1>(mypq4.top());
+									mypq4.pop();
+									mypq5.push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));						
+									return next;
+								}else{						
+									if(!mypq5.empty() and get<0>(mypq5.top()) < duracion){	
+										int next = get<1>(mypq5.top());
+										mypq5.pop();
+										mypq5.push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));						
+										return next;
+									}else{
+										return current_pid(cpu);
+									}
+								}								
+							}
+						}
+					}
+				}				
+						//por alguna razon no me reconoce el colas, por eso no andaba...debe ser un lio de contextos.Ahora creo que ya no necesitamos colas[] en el .h o el constructor, pero lo voy a dejar asi por ahora
+				/*for(int i =1; i <= prioridad; i++){
 					if(i == prioridad){
-						if( !colas[i-1].empty() and get<0>(colas[i-1].top()) < prioridad){
-							int next = get<1>(colas[i].top());
+						if( !colas[i-1].empty() and get<0>(colas[i-1].top()) < duracion){
+							int next = get<1>(colas[i-1].top());//decia i
 							colas[i-1].pop();
 							colas[prioridad-1].push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));
 							return next;
 						}else{
 							return current_pid(cpu);
 						}						
-					}//Si llega aca es porque es menor, osea que tiene mayor prioridad
-					if(!colas[i-1].empty()){
-						int next = get<1>(colas[i-1].top());
-						colas[i-1].pop();
-						return next;
-					}
-				}				
-				
+					}else{//Si llega aca es porque es menor, osea que tiene mayor prioridad
+						if((i < prioridad) and (colas[i-1].empty() == false)){
+							colas[prioridad-1].push(make_tuple((*(tsk_params(current_pid(cpu))))[0],current_pid(cpu)));
+							int next = get<1>(colas[i-1].top());
+							colas[i-1].pop();
+							return next;
+						}else{
+							return current_pid(cpu);
+						}
+					}	
+				}*/			
 			}
 
 		}
